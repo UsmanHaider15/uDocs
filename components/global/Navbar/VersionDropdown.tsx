7bc824eb21dcb08c5ee3f7156e898d0adefc1ce4
Usman Haider <leo14haider@gmail.com>
2024-01-05 21:28:31 +0500

    make version dropdown interactive

--- components/global/Navbar/VersionDropdown.tsx
+++ components/global/Navbar/VersionDropdown.tsx
@@ -1,21 +1,26 @@
 'use client'
-import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 interface VersionDropdownProps {
   lang: string
   version: string
 }
-const VersionDropdown = ({ lang, version }) => {
-  const [selectedVersion, setSelectedVersion] = useState('v1')
+
+const VersionDropdown = ({ lang, version }: VersionDropdownProps) => {
+  const router = useRouter()
 
   const versions = ['v1', 'v2']
+
+  const handleVersionChange = (e) => {
+    const newVersion = e.target.value
+    router.push(`/${lang}/docs/${newVersion}`)
+  }
 
   return (
     <select
       id="version-select"
       className="p-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      value={selectedVersion}
-      onChange={(e) => setSelectedVersion(e.target.value)}
+      onChange={handleVersionChange}
     >
       {versions.map((version, index) => (
         <option key={index} value={version}>
