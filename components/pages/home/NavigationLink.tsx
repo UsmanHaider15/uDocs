1579f4ef3d9305433c59df80964d4c090ec0233a
Usman Haider <leo14haider@gmail.com>
2024-01-25 19:39:09 +0500

    add selected link higlighting

--- components/pages/home/NavigationLink.tsx
+++ components/pages/home/NavigationLink.tsx
@@ -1,28 +1,37 @@
+'use client'
 import Link from 'next/link'
 import React, { FC } from 'react'
 import { TOCLink } from 'types'
+import { usePathname } from 'next/navigation'
 
 const NavigationLink: FC<{
   link: TOCLink
   language: string
   version: string
 }> = ({ link, language, version }) => {
+  const pathname = usePathname()
+  console.log(pathname)
+
   const hasNestedLinks = link.links && link.links.length > 0
+
+  // Construct the href for comparison
+  const href = `/${language}/docs/${version}/${link.slug}`
+
+  // Check if the current pathname matches the link's href
+  const isActive = pathname === href
 
   return (
     <div className={`mt-1 ${hasNestedLinks ? 'mb-1' : ''}`}>
-      {' '}
       <Link
         className={`block px-2 py-1 text-sm hover:bg-blue-50 hover:text-blue-700 ${
           hasNestedLinks ? 'font-semibold' : 'font-normal'
-        }`} // Reduced padding and smaller text size
-        href={`/${language}/docs/${version}/${link.slug}`}
+        } ${isActive ? 'bg-blue-100 text-blue-700' : ''}`} // Add conditional styling for active link
+        href={href}
       >
         {link.title}
       </Link>
       {hasNestedLinks && (
         <div className="ml-2">
-          {' '}
           {link.links?.map((nestedLink) => (
             <NavigationLink
               key={nestedLink.slug}
--- components/pages/home/Sidebar.tsx
+++ components/pages/home/Sidebar.tsx
@@ -1,8 +1,6 @@
-'use client'
-import React, { useState, FC } from 'react'
+import React, { FC } from 'react'
 import { TOCLink } from 'types'
 import NavigationLink from './NavigationLink'
-import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io'
 
 interface SidebarProps {
   toc: TOCLink
@@ -11,28 +9,10 @@ interface SidebarProps {
 }
 
 const Sidebar: FC<SidebarProps> = ({ toc, language, version }) => {
-  // State to manage the visibility of the sidebar on small screens
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-
   return (
     <div className="md:sticky md:top-[126px] md:h-[calc(100vh-121px)] md:w-[284px] md:flex md:shrink-0 md:flex-col md:justify-between border-b-2">
-      {/* Button to toggle sidebar visibility on small screens */}
-      <div className="w-full pb-2">
-        <button
-          className="md:hidden flex items-center justify-center"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        >
-          {isSidebarOpen ? <IoIosArrowDown /> : <IoIosArrowForward />}{' '}
-          <span>Menu</span>
-        </button>
-      </div>
-
       {/* Sidebar */}
-      <aside
-        className={`bg-white w-full md:w-64 min-h-screen border-r border-gray-200 p-2 transition-all ease-in-out duration-300 ${
-          isSidebarOpen ? 'w-64' : 'hidden md:block'
-        }`}
-      >
+      <aside className="bg-white w-full md:w-64 min-h-screen border-r border-gray-200 p-2">
         {toc.links &&
           toc.links.map((link) => (
             <NavigationLink
