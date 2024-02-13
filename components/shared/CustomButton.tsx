f499ce4ed75b4a7f9642ad71e7c316304aee1f29
Usman Haider <leo14haider@gmail.com>
2024-02-13 05:50:03 +0500

    fix button styling

--- components/shared/CustomButton.tsx
+++ components/shared/CustomButton.tsx
@@ -4,7 +4,7 @@ function ButtonComponent({ text, href, style }) {
   return (
     <button
       type="button"
-      className={`py-2 px-4 font-semibold rounded-lg shadow-md transition-colors duration-300 ease-in-out ${style}`}
+      className={style}
       onClick={() => window.open(href, '_blank')}
     >
       {text}
