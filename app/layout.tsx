d407065204614475f11c09865a794cab88fe8cf8
Usman Haider <leo14haider@gmail.com>
2024-03-16 16:29:39 +0500

    add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from './theme-provider'
 
 import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
